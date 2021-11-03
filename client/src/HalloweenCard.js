dd29c58994d186e9944c4de3b543c0afb6b95021
Quinn Lonergan <qui99lo@gmail.com>
2021-11-03 14:03:52 -0400

    added cards

--- client/src/HalloweenCard.js
+++ client/src/HalloweenCard.js
@@ -1,23 +1,47 @@
 import React, {useState} from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
 
 function HalloweenCard({name, image, person}) {
-  const [isInStock, setIsInStock] = useState(true)
 
-  function handleClick(){
-    setIsInStock((isInStock) => (!isInStock))
-  }
 
   return (
-    <li className="card">
-      <img src={image} alt={name} />
-      <h4>{name}</h4>
-      <p>Creator: {person}</p>
-      {isInStock ? (
-        <button onClick={handleClick} className="primary">In Stock</button>
-      ) : (
-        <button onClick={handleClick}>Out of Stock</button>
-      )}
-    </li>
+    <Grid item key={name} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image={image}
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {person}
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
   );
 }
 
