783ec6da2ee08d7a544332de2deaffe1cdfa2cd6
Quinn Lonergan <qui99lo@gmail.com>
2021-11-04 16:34:16 -0400

    removed unused item

--- client/src/HalloweenCard.js
+++ client/src/HalloweenCard.js
@@ -3,7 +3,6 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
