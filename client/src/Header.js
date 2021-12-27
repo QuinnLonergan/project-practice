6e63b230585553a0344dff705c773d3b9a3bc08f
Quinn Lonergan <qui99lo@gmail.com>
2021-12-27 12:12:06 -0500

    added dependencies

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,8 +1,23 @@
 import React from "react";
-import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Typography from '@mui/material/Typography';
+import { Link } from 'react-router-dom';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import List from '@mui/material/List';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemButton from '@mui/material/ListItemButton';
+import { ListSubheader } from '@mui/material';
+import Drawer from '@mui/material/Drawer';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
 
 
 function Header() {
